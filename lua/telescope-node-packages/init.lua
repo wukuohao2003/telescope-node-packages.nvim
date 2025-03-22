04b34cece1bfe5fe710cc7a6ffe1ed307716fe0d
Alan-Kuohao <152664179+wukuohao2003@users.noreply.github.com>
2025-03-22 10:41:20 +0800

    Change notify title & Do not close window

--- lua/telescope-node-packages/init.lua
+++ lua/telescope-node-packages/init.lua
@@ -7,150 +7,192 @@ local action_state = require("telescope.actions.state")
 local Job = require("plenary.job")
 
 local function get_package_json_path()
-    local current_dir = vim.fn.getcwd()
-    local package_json_path = current_dir .. "/package.json"
-    if vim.fn.filereadable(package_json_path) == 1 then
-        return package_json_path
-    end
-    return nil
+	local current_dir = vim.fn.getcwd()
+	local package_json_path = current_dir .. "/package.json"
+	if vim.fn.filereadable(package_json_path) == 1 then
+		return package_json_path
+	end
+	return nil
 end
 
 local function get_installed_packages()
-    local package_json_path = get_package_json_path()
-    if not package_json_path then
-        return {}
-    end
-    local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
-    local dependencies = package_json.dependencies or {}
-    local devDependencies = package_json.devDependencies or {}
-    local packages = {}
-    for name, _ in pairs(dependencies) do
-        table.insert(packages, name)
-    end
-    for name, _ in pairs(devDependencies) do
-        table.insert(packages, name)
-    end
-    return packages
+	local package_json_path = get_package_json_path()
+	if not package_json_path then
+		return {}
+	end
+	local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
+	local dependencies = package_json.dependencies or {}
+	local devDependencies = package_json.devDependencies or {}
+	local packages = {}
+	for name, _ in pairs(dependencies) do
+		table.insert(packages, name)
+	end
+	for name, _ in pairs(devDependencies) do
+		table.insert(packages, name)
+	end
+	return packages
 end
 
 local args = function(command, package_name)
-    if command == "yarn" then
-        return {
-            remove = { "remove", package_name },
-            install = { "add", package_name }
-        }
-    elseif command == "pnpm" then
-        return {
-            remove = { "remove", package_name },
-            install = { "add", package_name }
-        }
-    else
-        return {
-            remove = { "uninstall", package_name },
-            install = { "install", package_name }
-        }
-    end
+	if command == "yarn" then
+		return {
+			remove = { "remove", package_name },
+			install = { "add", package_name },
+		}
+	elseif command == "pnpm" then
+		return {
+			remove = { "remove", package_name },
+			install = { "add", package_name },
+		}
+	else
+		return {
+			remove = { "uninstall", package_name },
+			install = { "install", package_name },
+		}
+	end
 end
 
 local function remove_package(package_name)
-    local package_json_path = get_package_json_path()
-    if not package_json_path then
-        vim.notify('No package.json file found in the current directory', vim.log.levels.WARN)
-        return
-    end
-    local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
-    local command
-    if not package_json.node_packages or not package_json.node_packages.command then
-        vim.notify("No package manager found in package.json", vim.log.levels.ERROR)
-        return
-    else
-        command = package_json.node_packages.command
-    end
-    local args_cmd = args(command, package_name)
-    Job:new({
-        command = command,
-        args = args_cmd.remove,
-        on_exit = function(_, return_val)
-            if return_val == 0 then
-                vim.notify("Package " .. package_name .. " removed successfully")
-            else
-                vim.notify("Failed to remove package " .. package_name, vim.log.levels.WARN)
-            end
-        end,
-    }):start()
+	local package_json_path = get_package_json_path()
+	if not package_json_path then
+		vim.notify(
+			"No package.json file found in the current directory",
+			vim.log.levels.WARN,
+			{ title = "Telescope Node Packages" }
+		)
+		return
+	end
+	local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
+	local command
+	if not package_json.node_packages or not package_json.node_packages.command then
+		vim.notify(
+			"No package manager found in package.json",
+			vim.log.levels.ERROR,
+			{ title = "Telescope Node Packages" }
+		)
+		return
+	else
+		command = package_json.node_packages.command
+	end
+	local args_cmd = args(command, package_name)
+	Job:new({
+		command = command,
+		args = args_cmd.remove,
+		on_exit = function(_, return_val)
+			if return_val == 0 then
+				vim.notify(
+					"Package " .. package_name .. " removed successfully",
+					vim.log.levels.INFO,
+					{ title = "Telescope Node Packages" }
+				)
+			else
+				vim.notify(
+					"Failed to remove package " .. package_name,
+					vim.log.levels.WARN,
+					{ title = "Telescope Node Packages" }
+				)
+			end
+		end,
+	}):start()
 end
 
 local function install_packages(package_names)
-    local package_json_path = get_package_json_path()
-    if not package_json_path then
-        vim.notify('No package.json file found in the current directory', vim.log.levels.WARN)
-        return
-    end
-    local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
-    local command
-    if not package_json.node_packages or not package_json.node_packages.command then
-        vim.notify("No package manager found in package.json", vim.log.levels.WARN)
-        return
-    else
-        command = package_json.node_packages.command
-    end
-    local package_include = string.find(package_names, ",", 1, true) ~= nil
-    local packages
-    if package_include then
-        packages = { package_names }
-    else
-        packages = vim.split(package_names, ",", { trimempty = true })
-    end
-    for _, package_name in ipairs(packages) do
-        local args_cmd = args(command, package_name)
-        Job:new({
-            command = command,
-            args = args_cmd.install,
-            on_exit = function(_, return_val)
-                if return_val == 0 then
-                    vim.notify("Package " .. package_name .. " installed successfully")
-                else
-                    vim.notify("Failed to install package " .. package_name, vim.log.levels.ERROR)
-                end
-            end,
-        }):start()
-    end
+	local package_json_path = get_package_json_path()
+	if not package_json_path then
+		vim.notify(
+			"No package.json file found in the current directory",
+			vim.log.levels.WARN,
+			{ title = "Telescope Node Packages" }
+		)
+		return
+	end
+	local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
+	local command
+	if not package_json.node_packages or not package_json.node_packages.command then
+		vim.notify(
+			"No package manager found in package.json",
+			vim.log.levels.WARN,
+			{ title = "Telescope Node Packages" }
+		)
+		return
+	else
+		command = package_json.node_packages.command
+	end
+	local package_include = string.find(package_names, ",", 1, true) ~= nil
+	local packages
+	if package_include then
+		packages = { package_names }
+	else
+		packages = vim.split(package_names, ",", { trimempty = true })
+	end
+	for _, package_name in ipairs(packages) do
+		local args_cmd = args(command, package_name)
+		Job:new({
+			command = command,
+			args = args_cmd.install,
+			on_exit = function(_, return_val)
+				if return_val == 0 then
+					vim.notify(
+						"Package " .. package_name .. " installed successfully",
+						vim.log.levels.INFO,
+						{ title = "Telescope Node Packages" }
+					)
+				else
+					vim.notify(
+						"Failed to install package " .. package_name,
+						vim.log.levels.ERROR,
+						{ title = "Telescope Node Packages" }
+					)
+				end
+			end,
+		}):start()
+	end
 end
 
 function M.start()
-    local package_json_path = get_package_json_path()
-    if not package_json_path then
-        vim.notify('No package.json file found in the current directory', vim.log.levels.WARN)
-        return
-    end
+	local package_json_path = get_package_json_path()
+	if not package_json_path then
+		vim.notify(
+			"No package.json file found in the current directory",
+			vim.log.levels.WARN,
+			{ title = "Telescope Node Packages" }
+		)
+		return
+	end
 
-    local packages = get_installed_packages()
-    pickers.new({}, {
-        prompt_title = 'Node.js Packages',
-        finder = finders.new_table({
-            results = packages,
-        }),
-        sorter = conf.generic_sorter({}),
-        attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-                local selection = action_state.get_selected_entry()
-                actions.close(prompt_bufnr)
-                if selection then
-                    remove_package(selection[1])
-                end
-            end)
+	local packages = get_installed_packages()
+	pickers
+		.new({}, {
+			prompt_title = "Node.js Packages",
+			finder = finders.new_table({
+				results = packages,
+			}),
+			sorter = conf.generic_sorter({}),
+			attach_mappings = function(_, map)
+				actions.select_default:replace(function()
+					local selection = action_state.get_selected_entry()
+					if selection then
+						remove_package(selection[1])
+					end
+				end)
 
-            map('i', '<CR>', function()
-                local input = action_state.get_current_line()
-                actions.close(prompt_bufnr)
-                if input and #input > 0 then
-                    install_packages(input)
-                end
-            end)
+				map("i", "<CR>", function()
+					local input = action_state.get_current_line()
+					if input and #input > 0 then
+						install_packages(input)
+					else
+						vim.notify(
+							"The package name cannot be empty",
+							vim.log.levels.WARN,
+							{ title = "Telescope Node Packages" }
+						)
+					end
+				end)
 
-            return true
-        end,
-    }):find()
+				return true
+			end,
+		})
+		:find()
 end
 
 return M
